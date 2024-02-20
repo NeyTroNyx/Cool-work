0780d3f189263792ff5b45f02463b7fbd8d433a9
NeyTroNyx <145029010+NeyTroNyx@users.noreply.github.com>
2024-02-20 15:00:15 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -14,8 +14,10 @@ class FirstRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(backgroundColor: Color.fromARGB(255, 0, 130, 255),
       appBar: AppBar(
-        title: const Text('Колледж связи 54',style: TextStyle(color: Color.fromARGB(255, 0, 0, 0),   // зеленый цвет текста
-            fontSize: 40,),)
+        title: Center(
+          child: const Text('Вкусно и точка',style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), 
+              fontSize: 30,),),
+        )
       ),
       body: Center(
         child: ElevatedButton(
@@ -44,7 +46,7 @@ class SecondRoute extends StatelessWidget {
       body: Center(
         child: ConstrainedBox(
       constraints: 
-      BoxConstraints.tightFor(width: 400, height: 400),
+      BoxConstraints.tightFor(width: 300, height: 600),
         child: Column(
           children: [       
           Container(
@@ -71,7 +73,7 @@ class SecondRoute extends StatelessWidget {
             onPressed: () {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => const fourthRoute()),
+              MaterialPageRoute(builder: (context) => const FourthRoute()),
             );
           }       
         ),
@@ -115,7 +117,7 @@ class ThirdRoute extends StatelessWidget {
      body: Center(
         child: ConstrainedBox(
       constraints: 
-      BoxConstraints.tightFor(width: 400, height: 500),
+      BoxConstraints.tightFor(width: 300, height: 600),
         child: Column(
           children: [       
           Container(
@@ -172,40 +174,86 @@ class ThirdRoute extends StatelessWidget {
     );
   }
 }
-class fourthRoute extends StatelessWidget {
-  const fourthRoute({super.key});
+class FourthRoute extends StatelessWidget {
+  const FourthRoute({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(backgroundColor: Color.fromARGB(255, 0, 130, 255), 
-    body: Center(
-        child: ConstrainedBox(
-      constraints: 
-      BoxConstraints.tightFor(width: 500, height: 500),
-        child: Column(
-          children: [       
-          Container(
-      color: Color.fromARGB(255, 0, 130, 255),
-      alignment: Alignment.topLeft,
-      padding: EdgeInsets.all(15),
-      margin: EdgeInsets.only(top: 10),
-      child: Center(child: Text('Добро пожаловать!', style: TextStyle(color: Color.fromARGB(255, 0, 0, 0),
-            fontSize: 40,)),)
-    ),
-    Text (''),
-           ElevatedButton(
-          child: const Text('Назад'),
-          onPressed: () {
+    appBar: AppBar(
+        title: const Text('Вкусно и точка',style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), 
+            fontSize: 30,),)
+      ),
+      body: Center(
+        child: ListView(
+            children: [
+              Center(
+                child: ElevatedButton (style: ElevatedButton.styleFrom(
+    primary: Color.fromARGB(255, 0, 130, 255), 
+  ),child: Container(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        alignment: Alignment.topLeft,
+                        padding: EdgeInsets.all(15),
+                        margin: EdgeInsets.only(top:50),
+                        width: 300,
+                        height: 250,
+                         child: Column (
+                        children: [
+                         Container 
+                         (child: Image.network("https://thescottishsun.co.uk/wp-content/uploads/sites/2/2019/01/Burgers-Cheeseburger-L-e1547823525793.jpg"
+                         ),
+                         ),
+                         Text('Чизбургер')
+                         ]
+                         )
+                ),
+                onPressed: () {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => const FirstRoute()),
             );
-          },
+          }       
+                )
+              ),
+              Center(
+                child: ElevatedButton (style: ElevatedButton.styleFrom(
+    primary: Color.fromARGB(255, 0, 130, 255), 
+  ),child: Container(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        alignment: Alignment.topLeft,
+                        padding: EdgeInsets.all(15),
+                        margin: EdgeInsets.only(top:50),
+                        width: 300,
+                        height: 250,
+                         child: Column (
+                        children: [
+                         Container 
+                         (child: Image.network("https://thescottishsun.co.uk/wp-content/uploads/sites/2/2019/01/Burgers-Cheeseburger-L-e1547823525793.jpg"
+                         ),
+                         ),
+                         Text('Чизбургер')
+                         ]
+                         )
+                ),
+                onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const FirstRoute()),
+            );
+          }       
+                )
+              ),
+              
+            ],
         ),
-          ]
-        )
-        )
-    )
-    );
+      ),
+        );
   }
 }
+
+
+
+
+
+
+
